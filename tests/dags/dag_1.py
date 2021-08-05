c8b8b5d60ec0b2ca98e4e31fbef17c6405fe37c9
david jarzombek <jarzombekdavid@gmail.com>
2021-08-05 17:13:24 -0400

    asdf

--- tests/dags/dag_1.py
+++ tests/dags/dag_1.py
@@ -1,8 +1,9 @@
-
+import os
 from datetime import datetime
 
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 
 DEFAULT_DATE = datetime(2016, 1, 1)
 
@@ -11,5 +12,11 @@ default_args = {
     "start_date": DEFAULT_DATE,
 }
 
+def print_envs():
+    print(os.getenv('AWS_DEFAULT_REGION'))
+
 with DAG(dag_id="this_should_show_up", default_args=default_args, schedule_interval='@once') as dag:
-    task_a = DummyOperator(task_id="test_task_a")
+    task_a = PythonOperator(
+        task_id="test_task_a",
+        callable=print_envs
+        )
